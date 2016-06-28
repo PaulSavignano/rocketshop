6db2adbbd3808a13b6ff09c8d6e7ded839dc8be3
Paul Savignano <Paul.Savignano@gmail.com>
2016-06-27 18:59:02 -0700

    Added routes.

--- client/components/header.js
+++ client/components/header.js
@@ -1,28 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router'
 
-const Header = () => {
-  return (
-    <nav className="navbar navbar-inverse navbar-fixed-top">
-      <div className="container">
-        <div className="navbar-header">
-          <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <a className="navbar-brand" href="#">Project name</a>
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-inverse navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <Link className="navbar-brand" to="/">Rocket Shop</Link>
+          </div>
+          <div id="navbar" class="collapse navbar-collapse">
+            <ul className="nav navbar-nav">
+              <li className="active"><Link to="/products">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+          </div>
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul className="nav navbar-nav">
-            <li className="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
+  }
 }
 
 export default Header
